5e9a744171f9099b4210d75f5875d760c3cad9b6
Muhammad Ali <muhammadaliacc@gmail.com>
2026-03-04 03:56:35 +0100

    upload files

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,7 +1,15 @@
-// E:\Muhammad_Ali_Portfolio\portfolio\next.config.js
+// // E:\Muhammad_Ali_Portfolio\portfolio\next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  output: 'export',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
