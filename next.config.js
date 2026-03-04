fb1d6789a24bfa36237b1b481d6a9abd7f89408b
Muhammad Ali <muhammadaliacc@gmail.com>
2026-03-04 07:07:35 +0100

    upload

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
-// // E:\Muhammad_Ali_Portfolio\portfolio\next.config.js
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-
-// module.exports = nextConfig
-
-
+// E:\Muhammad_Ali_Portfolio\portfolio\next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/out',      // Optional: if you want all files under /out in S3
-  assetPrefix: '/out',   // Ensures _next assets use /out prefix
-};
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   basePath: '/out',      // Optional: if you want all files under /out in S3
+//   assetPrefix: '/out',   // Ensures _next assets use /out prefix
+// };
+
+// module.exports = nextConfig;
